800a9fd61a0dd557c7f4e7f1202633a598bac4f1
AlexAntipin <antipin-it@mail.ru>
2020-04-03 18:38:31 +0400

    AlexAntipin

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-" " 
+import vk_api
+import json
+from flask import Flask, request
+
+
+
+vk_session = vk_api.VkApi(token=API_TOKEN)
+api = vk_session.get_api()
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST"])
+def handling_post():
+  data = json.loads(request.data)
+  if 'type' not in data.keys():
+    return 'not vk request'
+  if data['type'] == 'confirmation':
+    return CONFIRMATION_TOKEN
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  app.run()
